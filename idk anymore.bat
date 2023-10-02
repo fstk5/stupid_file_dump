6c3c4162cc7183b47103a09b105b3f2d9f9cadb9
Lucas Williams <73551377+FastestK5@users.noreply.github.com>
2023-10-02 14:47:55 -0700

    working on finding key

--- idk anymore.bat
+++ idk anymore.bat
@@ -1,2 +1,4 @@
-Fpting file with PyArmor..."
+echo "Installing PyArmor..."
+pip install pyarmor
+echo "Encrypting file with PyArmor..."
 pyarmor gen --private --enable-rft --enable-bcc --enable-jit --enable-themida --assert-call --obf-code 2 yp.py
